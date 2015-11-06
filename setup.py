de61ac8fa1483fc740c82310048c48a2284283e5
Daniel Fairhead <daniel.fairhead@om.org>
2015-11-06 13:40:32 +0000

    version bump, and fix __version__ import issue. twas a nice idea, I thought once.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
-VERSION = '0.9.4'
+VERSION = '0.9.5'
 
 setup(
     name = 'finder_colors',
     py_modules = ['finder_colors'],
     scripts = ['finder_colors.py'],
-    version = __version__,
+    version = VERSION,
     description = 'Get/Set the Colors set on files by OSX Finder',
     long_description=open('README.rst','r').read(),
     author = 'Daniel Fairhead',
     author_email = 'danthedeckie@gmail.com',
     url = 'https://github.com/danthedeckie/finder_colors',
-    download_url = 'https://github.com/danthedeckie/finder_colors/tarball/' + __version__,
+    download_url = 'https://github.com/danthedeckie/finder_colors/tarball/' + VERSION,
     keywords = ['OSX', 'OS X', 'Finder', 'Colors', 'Utility', 'Colours'],
     install_requires = ['xattr', ],
     classifiers = ['Development Status :: 4 - Beta',
